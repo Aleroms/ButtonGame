a6b326969a699700ff440cbf3d39ac70f6fc6338
Santiago <santiagomorales333@yahoo.com>
2022-12-31 10:09:38 -0800

    started backend for login

--- Assets/Scripts/Login.cs
+++ Assets/Scripts/Login.cs
@@ -14,13 +14,14 @@ public class Login : MonoBehaviour
     [Header("Login Variables")]
     [SerializeField] private TextMeshProUGUI login_uname;
     [SerializeField] private TextMeshProUGUI login_passwrd;
+    [SerializeField] private TextMeshProUGUI login_feedback;
 
 
     [Header("Register Variables")]
     [SerializeField] private TMP_InputField reg_uname;
     [SerializeField] private TMP_InputField reg_email;
     [SerializeField] private TMP_InputField reg_passwrd;
-    [SerializeField] private TextMeshProUGUI feedback;
+    [SerializeField] private TextMeshProUGUI reg_feedback;
 
     private Color feedback_color;
     private Color success_green;
@@ -28,10 +29,13 @@ public class Login : MonoBehaviour
     private void Start()
     {
         register_button = GameObject.FindGameObjectWithTag("register-button").GetComponent<Button>();
-        
+
         //clear the feedback @ start
-        feedback.text = "";
-        feedback_color = feedback.color;
+        login_feedback.text = "";
+        reg_feedback.text = "";
+        
+        //both login and reg share same color; save it
+        feedback_color = reg_feedback.color;
         success_green = new Color(25f, 135f, 84f);
     }
 
@@ -76,13 +80,45 @@ public class Login : MonoBehaviour
     }
     private void ErrorMessage(string msg)
     {
-        feedback.text = msg;
-        feedback.color = Color.red;
+        reg_feedback.text = msg;
+        reg_feedback.color = Color.red;
+        login_feedback.text = msg;
+        login_feedback.color = Color.red;
     }
     private void FeedbackMessage(string msg, Color c)
     {
-        feedback.color = c;
-        feedback.text = msg;
+        reg_feedback.color = c;
+        reg_feedback.text = msg;
+        login_feedback.color = c;
+        login_feedback.text = msg;
+    }
+    private void DatabaseMessage(string downloadHandler)
+    {
+        //Error Codes
+        // 1 - database connection error
+        // 2 - usernamecheck query error
+        // 3 - user already exists
+        // 4 - emailcheck query error
+        // 5 - email already exists
+        // 6 - insert query error
+
+        switch (downloadHandler)
+        {
+            case "0":
+                //No Errors
+                FeedbackMessage("Success!", Color.green);
+                Debug.Log("no errors from database.");
+                break;
+            case "3":
+                ErrorMessage("user already exists.");
+                break;
+            case "5":
+                ErrorMessage("email already exists.");
+                break;
+            default:
+                ErrorMessage("server-side error. Please try again.");
+                break;
+        }
     }
     private IEnumerator RegisterPost()
     {
@@ -100,32 +136,7 @@ public class Login : MonoBehaviour
         //if no errors
         if(post.downloadHandler.error == "")
         {
-            //Error Codes
-            // 1 - database connection error
-            // 2 - usernamecheck query error
-            // 3 - user already exists
-            // 4 - emailcheck query error
-            // 5 - email already exists
-            // 6 - insert query error
-
-            switch (post.downloadHandler.text)
-            {
-                case "0":
-                    //No Errors
-                    FeedbackMessage("Success!", Color.green);
-                    Debug.Log("no errors from database.");
-                    break;
-                case "3":
-                    ErrorMessage("user already exists.");
-                    break;
-                case "5":
-                    ErrorMessage("email already exists.");
-                    break;
-                default:
-                    ErrorMessage("server-side error. Please try again.");
-                    break;
-            }
-            
+            DatabaseMessage(post.downloadHandler.text);
             Debug.Log(post.downloadHandler.text);
         }
         else
@@ -136,6 +147,17 @@ public class Login : MonoBehaviour
 
         //release locked register button so player can resubmit
         register_button.enabled = true;
+    }
+    private IEnumerator LoginPost()
+    {
+        WWWForm form = new WWWForm();
+
+        form.AddField("username", reg_uname.text);
+        form.AddField("password", reg_passwrd.text);
+        form.AddField("unity-api-key", "SantiagoGames");
+
+        UnityWebRequest post = UnityWebRequest.Post("http://localhost/Udemy/login.php", form);
+        yield return post.SendWebRequest();
     }
     /*====================== Public Methods Below ===========================*/
     public void UserRegister()
@@ -167,7 +189,7 @@ public class Login : MonoBehaviour
             Debug.Log("password failed");
         else
         {
-            //StartCoroutine(LoginPost());
+            StartCoroutine(LoginPost());
             FeedbackMessage("logging in...", feedback_color);
         }
     }
@@ -175,7 +197,8 @@ public class Login : MonoBehaviour
     //======Reset Below============
     public void ResetFeedback()
     {
-        feedback.text = "";
+        reg_feedback.text = "";
+        login_feedback.text = "";
     }
     public void ResetForm()
     {
@@ -183,7 +206,10 @@ public class Login : MonoBehaviour
         reg_email.text = "";
         reg_passwrd.text = "";
         reg_uname.text = "";
-        
+
+        login_passwrd.text = "";
+        login_uname.text = "";
+
     }
     
 }
